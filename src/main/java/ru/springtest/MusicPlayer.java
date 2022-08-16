407cac12e89e9ccf33bd33fc6be06cceb800b652
Alexandr <narvyshalexandr@gmail.com>
2022-08-16 14:01:23 +0300

    add List

--- src/main/java/ru/springtest/MusicPlayer.java
+++ src/main/java/ru/springtest/MusicPlayer.java
@@ -1,7 +1,11 @@
 package ru.springtest;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
@@ -9,6 +13,7 @@ public class MusicPlayer {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -16,23 +21,21 @@ public class MusicPlayer {
     public int getVolume() {
         return volume;
     }
+
     public void setVolume(int volume) {
         this.volume = volume;
     }
 
-    //IoC
-    public MusicPlayer(Music music){
-        this.music = music;
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public MusicPlayer() {
-    }
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
 
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic(){
-        System.out.println("Playing: "+ music.getSong());
     }
 }
+
--- src/main/java/ru/springtest/TestSpring.java
+++ src/main/java/ru/springtest/TestSpring.java
@@ -11,7 +11,7 @@ public class TestSpring {
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        musicPlayer.playMusicList();
 
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
