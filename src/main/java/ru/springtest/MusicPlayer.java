4b7586394f284a19ae3da123072a56b45122f2e7
Alexandr <narvyshalexandr@gmail.com>
2022-08-22 16:02:31 +0300

    Autowired

--- src/main/java/ru/springtest/MusicPlayer.java
+++ src/main/java/ru/springtest/MusicPlayer.java
@@ -1,21 +1,27 @@
 package ru.springtest;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
-    private Music music;
+    private RockMusic rockMusic;
 
     public MusicPlayer(){
     }
-    public MusicPlayer(Music music) {
-        this.music = music;
+
+    @Autowired
+    public MusicPlayer(RockMusic rockMusic) {
+        this.rockMusic = rockMusic;
     }
 
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + rockMusic.getSong());
 
 
     }
--- src/main/java/ru/springtest/TestSpring.java
+++ src/main/java/ru/springtest/TestSpring.java
@@ -7,21 +7,8 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 
-        //Music music = context.getBean("musicBean", Music.class);
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
-
-        Music music = context.getBean("folkMusic", Music.class);
-
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
-
-        Music music1 = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
-        musicPlayer1.playMusic();
-
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-        musicPlayer2.playMusic();
 
 
         context.close();
